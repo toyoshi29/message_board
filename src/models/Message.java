be5e3fa230c4b4b9755262efec9aa17e0c22b771
toyoshi <toyoshi29@yahoo.co.jp>
2020-10-10 19:57:41 +0900

    Add ShowServlet.java & show.jsp Modified Message.java

--- src/models/Message.java
+++ src/models/Message.java
@@ -36,7 +36,7 @@ public class Message {
     private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
-    private Timestamp update_at;
+    private Timestamp updated_at;
 
     // getter, setter
     public Integer getId() {
@@ -71,12 +71,12 @@ public class Message {
         this.created_at = created_at;
     }
 
-    public Timestamp getUpdate_at() {
-        return update_at;
+    public Timestamp getUpdated_at() {
+        return updated_at;
     }
 
-    public void setUpdate_at(Timestamp update_at) {
-        this.update_at = update_at;
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
 
 }
